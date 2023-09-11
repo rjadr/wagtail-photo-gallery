287a085a0fcaa3d534b782935a20b6f7ba3217da
RJ Adriaansen <adriaansen@eshcc.eur.nl>
2023-09-11 23:15:23 +0200

    Fix for Wagtail 5.0

--- demo/home/models.py
+++ demo/home/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from wagtail.models import Page
 
-from wagtail.core.fields import StreamField
+from wagtail.fields import StreamField
 from wagtail.admin.panels import FieldPanel
 
 from wagtail_photo_gallery.models import GalleryBlock, ImageGalleryMixin
